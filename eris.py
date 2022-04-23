b711842bef21c662031fc6bdaa0f9f84ed9b0bbc
zacharyfurbush <zacharyfurbush@gmail.com>
2022-04-23 14:04:55 -0400

    Testing Lightbulb

--- eris.py
+++ eris.py
@@ -1,9 +1,17 @@
 import os
 import hikari
+import lightbulb
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
+GUILD_ID = os.getenv('GUILD_ID')
+bot = lightbulb.BotApp(token = TOKEN, default_enabled_guilds=(GUILD_ID))
 
-bot = hikari.GatewayBot(token = TOKEN)
+@bot.command
+@lightbulb.command('ping', 'Says pong!')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):
+    await ctx.respond('Pong!')
+
 bot.run()
