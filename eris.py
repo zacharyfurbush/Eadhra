f38ef25d75cb95adba7874b5cc6080b82b34b7a0
zacharyfurbush <zacharyfurbush@gmail.com>
2022-05-22 16:48:56 -0400

    Converted to discord.py

--- eris.py
+++ eris.py
@@ -1,17 +1,13 @@
 import os
-import hikari
-import lightbulb
 from dotenv import load_dotenv
-
+import discord
+from discord.ext import commands
 load_dotenv()
-TOKEN = os.getenv('TOKEN')
-GUILD_ID = os.getenv('GUILD_ID')
-bot = lightbulb.BotApp(token = TOKEN, default_enabled_guilds=(GUILD_ID))
 
-@bot.command
-@lightbulb.command('ping', 'Says pong!')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def ping(ctx):
-    await ctx.respond('Pong!')
+bot = discord.Client()
 
-bot.run()
+@bot.event
+async def on_ready():
+    print('Eris is online!')
+
+bot.run(os.getenv('TOKEN'))
