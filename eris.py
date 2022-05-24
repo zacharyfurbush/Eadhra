1a12c84e7da9d6f0137510600697e48e754ab1b3
zacharyfurbush <zacharyfurbush@gmail.com>
2022-05-23 22:13:24 -0400

    thot bot testing

--- eris.py
+++ eris.py
@@ -1,13 +1,35 @@
 import os
 from dotenv import load_dotenv
+import pymongo
+from pymongo import mongo_client
 import discord
 from discord.ext import commands
-load_dotenv()
 
+# Private Stuff Retrieval
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+TESTGUILD = os.getenv('TESTGUILD')
+MONGOCONNECT = os.getenv('MONGOCONNECT')
+
+# MongoDB Connection
+cluster = mongo_client(MONGOCONNECT)
+db = cluster['ErisDB']
+levels_collection = db['Levels']
+
+# Bot Startup
 bot = discord.Client()
 
-@bot.event
+@bot.event 
 async def on_ready():
-    print('Eris is online!')
+    print('Eris is online!') # Startup Console Message
 
-bot.run(os.getenv('TOKEN'))
+@bot.event
+async def on_message(ctx):
+    author_id = ctx.author.id
+    guild_id = ctx.guild.id
+
+    # Thot Bot
+    if ctx.message('thot'):
+        print('oh no')
+
+bot.run(TOKEN)
