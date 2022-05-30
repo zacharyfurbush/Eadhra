899e36e95d4484666729f9b37fbb14a949181591
zacharyfurbush <zacharyfurbush@gmail.com>
2022-05-30 14:17:03 -0400

    Testing Command Tree

--- bot/eris.py
+++ bot/eris.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import pymongo
 from pymongo import MongoClient
 import discord
-from discord.ext import commands
+from discord import app_commands # refer https://github.com/Digiwind/Digiwind-Videos/blob/main/DPY%20Slash%20Commands.py
 
 # Private Stuff Retrieval
 load_dotenv()
@@ -16,27 +16,51 @@ cluster = MongoClient(MONGOCONNECT)
 db = cluster['ErisDB']
 levels_collection = db['Levels']
 
-# Bot Startup
-bot = discord.Client()
 
-# Startup Console Message
-@bot.event 
-async def on_ready():
-    print('Eris is online!') 
+class aclient(discord.Client):
+    def __init__(self):
+        super().__init__(intents = discord.Intents.all())
+        self.synced = False
 
-# Registers and Processes Messages Sent
-@bot.event
-async def on_message(ctx):
-    author_id = ctx.author.id
-    guild_id = ctx.guild.id
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:
+            await tree.sync(guild = discord.Object(id=TESTGUILD))
+            self.synced = True
+        print('Eris is online!') 
 
-    # If the message author is Eris, it wont reply
-    if author_id != '924400948707745802': 
+client = aclient()
+tree = app_commands.CommandTree(client)
 
-        # Thot Bot v2
-        if 'thot' in ctx.content.lower():
-            await ctx.reply('https://i.imgur.com/RoJ9IYz.jpg')
+@tree.command(name = 'Ping', description = 'Pings Eris', id = TESTGUILD)
+async def slash(interaction: discord.Interaction):
+    await interaction.response.send_message('Pong! 🏓', ephemeral = True)
+
+client.run(TOKEN)
+
+# # Bot Startup
+# bot = discord.Client()
+
+# # Startup Console Message
+# @bot.event 
+# async def on_ready():
+#     print('Eris is online!') 
+
+# # Registers and Processes Messages Sent
+# @bot.event
+# async def on_message(ctx):
+#     if ctx.author.bot: # Author Bot Check
+#         return
+
+#     # Thot Bot: Revival
+#     if 'thot' in ctx.content.lower():
+#         await ctx.reply('https://i.imgur.com/RoJ9IYz.jpg')
+
+#     author_id = ctx.author.id
+#     guild_id = ctx.guild.id
+
+# @bot.slash_command
         
 
 
-bot.run(TOKEN)
+# bot.run(TOKEN)
